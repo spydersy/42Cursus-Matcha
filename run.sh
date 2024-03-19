360d1cba2dddb4931f0f18b59b6178c4a78bb21e
spydersy <acbe1325@aol.com>
2024-03-19 18:29:38 +0000

    [ADDED] : run.sh script

---
+++ run.sh
@@ -0,0 +1,11 @@
+echo "Setting virtual environment"
+cd api && python3 -m venv .venv
+
+echo "Activate virtual environment"
+. .venv/bin/activate
+
+echo "Install Flask"
+pip install Flask
+
+echo "Start Application"
+cd app && flask --appmain run --debug
